de70170656b782d8dd24e3fe394dfde7280cc93f
Alanna Soepler <N00192732@iadt.ie>
2022-03-16 14:57:35 +0000

    Report

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,6 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
 Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('admin/home', [App\Http\Controllers\HomeController::class, 'index'])->name('admin.home');
